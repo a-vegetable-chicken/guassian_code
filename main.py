684cd6c4ed804da4aedc3ae39e1d5b943de87f89
wangtingyu <tingyuwangx@gmail.com>
2025-03-04 15:04:29 +0100

    1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import blenderproc as bproc
 import numpy as np
+import os
 
 data_path = "/scratch_net/biwidl311/wty/HM3D"
 info_txt_path = "./_info.txt"
@@ -26,7 +27,7 @@ bvh_tree = bproc.object.create_bvh_tree_multi_objects([objects, floor])
 poses = 0
 for try_counter in range(10000):
     location = bproc.sampler.upper_region([floor], min_height=1.5, max_height=1.8)
-    #print(location)
+   
     # Check that there is no object between the sampled point and the floor
     _, _, _, _, hit_object, _ = bproc.object.scene_ray_cast(location, [0, 0, -1])
     if hit_object != floor:
@@ -43,10 +44,27 @@ for try_counter in range(10000):
 
     # If all checks were passed, add the camera pose
     bproc.camera.add_camera_pose(cam2world_matrix)
-    
-    print(cam2world_matrix)
 
     poses += 1
-
-    if poses == 10 :
+    if poses == 5:
         break
+
+    
+color_intr_3x3 = intrinsics["color"][:3, :3]
+bproc.camera.set_intrinsics_from_K_matrix(color_intr_3x3, image_width=960, image_height=540)
+
+bproc.renderer.enable_depth_output(activate_antialiasing=False)
+bproc.renderer.set_max_amount_of_samples(50)
+
+data = bproc.renderer.render()
+
+
+output_dir = "../output"
+
+os.makedirs(output_dir, exist_ok=True)
+
+bproc.writer.write_hdf5(output_dir, data, append_to_existing_output=False)
+
+bproc.writer.write_png(output_dir, data, color_depth="8")
+
+print(f"render {poses} and output to: {output_dir}")
