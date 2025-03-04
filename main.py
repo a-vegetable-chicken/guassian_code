6e7fe279a546dd7feca1d3d32d14990a94843336
wangtingyu <tingyuwangx@gmail.com>
2025-03-04 16:37:18 +0100

    1

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ for try_counter in range(10000):
 
     # If all checks were passed, add the camera pose
     bproc.camera.add_camera_pose(cam2world_matrix)
-
+    print(bproc.camera.get_camera_pose())
     poses += 1
     if poses == 5:
         break
